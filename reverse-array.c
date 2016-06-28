98bbcd41e567cd0ac50ac5c6113d482e0c654073
aadams22 <ashleigh.adams22@gmail.com>
2016-06-28 17:27:49 -0500

    tabs are weird

--- reverse-array.c
+++ reverse-array.c
@@ -5,44 +5,42 @@ void reverses();
 int a[16];
 int i, j, k;
 
-
 int main() {
 
+for (int i = 0; i <= 16; ++i) 	// writes array
+{
+	a[i] = i;
+	printf("Element[%d] = %d\n", i, a[i] ); // output each array element's value
+}
 
-	for (int i = 0; i <= 16; ++i) 	// writes array
-	{
-		a[i] = i;
-		printf("Element[%d] = %d\n", i, a[i] ); // output each array element's value
-	}
+int k = sizeof(a);
+printf("%d\n", k);
 
-	int k = sizeof(a);
-	printf("%d\n", k);
+reverses(a); 	//reverses array
 
-	reverses(a); 	//reverses array
-
-	for (int i = 0; i <= 16; ++i)
-	{
-		printf("Element[%d] = %d\n", i, a[i] ); // output each array element's value
-
-	}
-
-	return 0;
+for (int i = 0; i <= 16; ++i)
+{
+	printf("Element[%d] = %d\n", i, a[i] ); // output each array element's value
 
 }
 
-	// function definition 
-	void reverses(int a[16]) {
+return 0;
+
+}
+
+// function definition 
+void reverses(int a[16]) {
 
 
-	for (int j = 0; j < 16/2; j++ ) 
-	{
-		int other = 16 - j;
-		int temp = a[j];
-		a[j] = a[other];
-		a[other] = temp;
-	}
+for (int j = 0; j < 16/2; j++ ) 
+{
+	int other = 16 - j;
+	int temp = a[j];
+	a[j] = a[other];
+	a[other] = temp;
+}
 
 
 
-	}
+}
 
