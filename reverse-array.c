1dd1d29945d46b5133742d10c68316ff249833ae
aadams22 <ashleigh.adams22@gmail.com>
2016-06-28 12:27:18 -0500

    init commit

--- reverse-array.c
+++ reverse-array.c
@@ -2,8 +2,8 @@
 
 //reversal function felclaration 
 void reverses();
-int n[12];
-int i, j, k;
+int n[16];
+int i, j;
 
 
 int main() {
@@ -22,22 +22,21 @@ int main() {
 	{
 		n[i] = i;
 		// output each array element's value
-		// printf("Element[%d] = %d\n", i, n[i] );
+		printf("Element[%d] = %d\n", i, n[i] );
 	}
 
 
-	for (int j = 0; j < 12/2; j++ ) 
+	for (int j = 0; j < 16/2; j++ ) 
 	{
-		//n[j]: 0, 1, 2, 3, 4, 5
-		int other = 12 - j - 1;
-		//other: 11, 10, 9, 8, 7, 6
+
+		int other = 16 - j - 1;
+
 		int temp = n[j];
-		//temp: 0, 1, 2, 3, 4, 5
+
 		n[j] = n[other];
-		//n[other] 11, 10, 9, 8, 7, 6
+
 		n[other] = temp;
-		//n[other] 0, 1, 2, 3, 4, 5
-		printf("%d\n", n[other]);
+
 	}
 
 
