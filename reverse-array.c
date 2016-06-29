a54bed918db8da4dced27857253e19d7e1fd225b
aadams22 <ashleigh.adams22@gmail.com>
2016-06-28 19:53:30 -0500

    abstracted out to allow for user to type number into command line

--- reverse-array.c
+++ reverse-array.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 //variable definitions
-#define ARR_LENGTH_K 32
+// #define ARR_LENGTH_K 32
 
 //function declaration 
 void reverse(int a[], size_t len);
@@ -10,27 +11,32 @@ void reverse(int a[], size_t len);
 // argc = argument count
 // argv = argument vector
 int main(int argc, char** argv) {
-	int a[ARR_LENGTH_K];
-	unsigned int i;
-
-
-	for(i = 0; i < argc; i++) {
-	    printf("arg %d is %s\n", i, argv[i]);
+	for(int i = 0; i < argc; i++) {
+	  printf("arg %d is %s\n", i, argv[i]);
 	}
 	
 
-	for (i = 0; i <= ARR_LENGTH_K; ++i) 	// writes array
+	char* ptr;
+
+  unsigned int length_k = strtol(argv[1], &ptr, 10);
+    
+	int a[length_k];
+	
+
+
+
+	for (unsigned int i = 0; i <= length_k; ++i) 	// writes array
 	{
 		a[i] = i;
 		// printf("Element[%d] = %d\n", i, a[i] ); // output each array element's value
 	}
 
 	int k = sizeof(a);
-	printf("%d\n", k);
+	printf("sizeof k %d\n", k);
 
-	reverse(a, ARR_LENGTH_K); 	//reverses array
+	reverse(a, length_k); 	//reverses array
 
-	for (i = 0; i <= ARR_LENGTH_K; ++i)
+	for (unsigned int i = 0; i <= length_k; ++i)
 	{
 		printf("Element[%d] = %d\n", i, a[i] ); // output each array element's value
 
@@ -55,4 +61,12 @@ void reverse(int a[], size_t len) {
 
 
 }
+
+
+// This bug exists when the user types 0: 
+/* ./a.out 0
+* arg 0 is ./a.out
+* arg 1 is 0
+* sizeof k 0
+/* Element[0] = 0
 
