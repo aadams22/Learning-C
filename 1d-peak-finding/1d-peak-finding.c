634b106eea1c3654a0411b939b848248a8a55205
aadams22 <ashleigh.adams22@gmail.com>
2016-07-23 13:17:17 +0900

    adding a divide and concquor solution for peak finding

--- 1d-peak-finding/1d-peak-finding.c
+++ 1d-peak-finding/1d-peak-finding.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 //function declaration
-void peak1D(int a[], size_t len);
+void bruteForce(int a[], size_t len);
+void divideAndConquor(int a[], int n);
 
 
 int main(int argc, char** argv) {
@@ -16,6 +17,7 @@ int main(int argc, char** argv) {
 	char* ptr;
 	unsigned int length_k = strtol(argv[1], &ptr, 10);
 	int a[length_k];
+	int n = (length_k - 1)/2;
 
 	for (int i = 0; i < length_k; ++i)
 	{
@@ -23,15 +25,16 @@ int main(int argc, char** argv) {
 		// printf("Element[%d] = %d\n", i, a[i] ); // output each array element's value
 	}
 
-	peak1D(a, length_k);
+	// bruteForce(a, length_k);
+	divideAndConquor(a, n);
 
 }
 
 
 //function definition
-void peak1D(int a[], size_t len) {
-	printf("length is %zu\n", len);
 
+//for loop version
+void bruteForce(int a[], size_t len) {
 
 	for (unsigned int i = 1; i < len; ++i)
 	{
@@ -45,3 +48,26 @@ void peak1D(int a[], size_t len) {
 	}
 	
 }
+
+//recursion version
+void divideAndConquor(int a[], int n) {
+	printf("this is a[%u]\n", a[n]);
+
+	if(a[n] > a[n - 1] && a[n] > a[n + 1]) {
+		printf("%u is a peak\n", a[n]);
+	}else if(a[n - 1] > a[n])
+	{
+		n = n - 1;
+		divideAndConquor(a, n);
+	}else {
+		n = n + 1;
+		divideAndConquor(a, n);
+	}
+}
+
+
+
+
+
+
+
